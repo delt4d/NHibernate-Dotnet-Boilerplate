c0f3b402010087100a068c414f280409af18e78f
Leandro Cruz <LeandroCruz717@outlook.com>
2024-01-05 15:44:47 -0300

    chore: create folder Entities and move Example entity to it

---
+++ NDB.Domain/Entities/Example.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NDB.Domain.Entities
+{
+    public class Example : IExample
+    {
+        public int Id { get; set; }
+        public string UUID { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+    }
+}

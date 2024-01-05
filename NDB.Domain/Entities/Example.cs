8b4de256ec3769bdf44c18f7f2f707fcbeaff468
Leandro Cruz <LeandroCruz717@outlook.com>
2024-01-05 15:45:50 -0300

    chore: put IExample interface and Example class in the same file

--- NDB.Domain/Entities/Example.cs
+++ NDB.Domain/Entities/Example.cs
@@ -2,6 +2,16 @@
 
 namespace NDB.Domain.Entities
 {
+    public interface IExample
+    {
+        int Id { get; set; }
+        string UUID { get; set; }
+        string Title { get; set; }
+        string Content { get; set; }
+        DateTime CreatedAt { get; set; }
+        DateTime? UpdatedAt { get; set; }
+    }
+
     public class Example : IExample
     {
         public int Id { get; set; }

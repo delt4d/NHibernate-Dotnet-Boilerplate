572c010550c1e02393e3885ee4e7876744c29e8a
Leandro Cruz <LeandroCruz717@outlook.com>
2024-01-05 17:25:29 -0300

    feat(infra): create Example entity nhibernate mapping

---
+++ NDB.Infra/Entities/NhibernateExample.cs
@@ -0,0 +1,15 @@
+﻿using NDB.Domain.Values;
+
+namespace NDB.Infra.Entities
+{
+    public class NhibernateExample : IExample
+    {
+        public virtual int Id { get; set; }
+        public virtual string UUID { get; set; }
+        public virtual string Title { get; set; }
+        public virtual ExampleContent Content { get; set; }
+        public virtual DateTime CreatedAt { get; set; }
+        public virtual DateTime? UpdatedAt { get; set; }
+        public virtual string Search { get; set; }
+    }
+}
--- NDB.Infra/GlobalUsings.cs
+++ NDB.Infra/GlobalUsings.cs
@@ -1,4 +1,5 @@
-﻿global using NDB.Domain;
+﻿global using NDB.Domain.Entities;
+global using NDB.Infra.Entities;
 global using NHibernate;
 global using FluentNHibernate.Mapping;
 global using NHibernate.Tool.hbm2ddl;

301e099939082a88d3f1808c8a3fa8e6af51524a
Leandro Cruz <LeandroCruz717@outlook.com>
2024-01-05 15:08:33 -0300

    feat: add methods to add dependency injection and create session factory for nhibernate

---
+++ NDB.Infra/DependencyInjection.cs
@@ -0,0 +1,31 @@
+﻿using FluentNHibernate.Cfg.Db;
+using FluentNHibernate.Cfg;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace NDB.Infra
+{
+    public static class DependencyInjection
+    {
+        public static readonly string ConnectionString = "Server=localhost,1433;Database=todo;User Id=sqluser;Password=1234;";
+
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        {
+            services.AddSingleton(GetSessionFactory());
+            services.AddScoped(provider => provider.GetService<ISessionFactory?>()!.OpenSession());
+            return services;
+        }
+
+        public static ISessionFactory GetSessionFactory()
+        {
+            return Fluently
+                .Configure()
+                .Database(MsSqlConfiguration.MsSql2012
+                    .ConnectionString(ConnectionString)
+                    .ShowSql()
+                    .FormatSql())
+                .Mappings(m => m.FluentMappings.AddFromAssembly(typeof(DependencyInjection).Assembly))
+                .ExposeConfiguration(cfg => new SchemaExport(cfg).Create(true, true))
+                .BuildSessionFactory();
+        }
+    }
+}
---
+++ NDB.Infra/GlobalUsings.cs
@@ -0,0 +1,4 @@
+﻿global using NDB.Domain;
+global using NHibernate;
+global using FluentNHibernate.Mapping;
+global using NHibernate.Tool.hbm2ddl;

fc60d7b947cd58d624e51d88b42bcab5f2119e54
Leandro Cruz <LeandroCruz717@outlook.com>
2024-01-05 22:56:59 -0300

    feat(infra): add InMemory database connection for testing

--- NDB.Infra/DependencyInjection.cs
+++ NDB.Infra/DependencyInjection.cs
@@ -6,21 +6,32 @@ namespace NDB.Infra
 {
     public static class DependencyInjection
     {
-        public static readonly string ConnectionString = "Server=localhost,1433;Database=todo;User Id=sqluser;Password=1234;";
-
         public static IServiceCollection AddInfrastructure(this IServiceCollection services)
         {
-            services.AddSingleton(GetSessionFactory());
+            services.AddSingleton(!UnitTestingDetector.IsRunningInUnitTest ? GetSqlServerSessionFactory() : GetInMemorySessionFactory());
             services.AddScoped(provider => provider.GetService<ISessionFactory?>()!.OpenSession());
             return services;
         }
 
-        public static ISessionFactory GetSessionFactory()
+        public static ISessionFactory GetInMemorySessionFactory()
+        {
+            return Fluently
+                .Configure()
+                .Database(SQLiteConfiguration.Standard
+                    .ConnectionString("Data Source=:memory:;Version=3;New=True;")
+                    .ShowSql()
+                    .FormatSql())
+                .Mappings(m => m.FluentMappings.AddFromAssembly(typeof(DependencyInjection).Assembly))
+                .ExposeConfiguration(cfg => new SchemaExport(cfg).Create(true, true))
+                .BuildSessionFactory();
+        }
+
+        public static ISessionFactory GetSqlServerSessionFactory()
         {
             return Fluently
                 .Configure()
                 .Database(MsSqlConfiguration.MsSql2012
-                    .ConnectionString(ConnectionString)
+                    .ConnectionString("Server=localhost,1433;Database=example;User Id=sqluser;Password=1234;")
                     .ShowSql()
                     .FormatSql())
                 .Mappings(m => m.FluentMappings.AddFromAssembly(typeof(DependencyInjection).Assembly))
---
+++ NDB.Infra/UnitTestingDetector.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+
+namespace NDB.Infra
+{
+    public class UnitTestingDetector
+    {
+        private static readonly HashSet<string> UnitTestingAttributes;
+
+        static UnitTestingDetector()
+        {
+            UnitTestingAttributes = [
+                "Microsoft.VisualStudio.TestTools.UnitTesting.TestClassAttribute",
+                "NUnit.Framework.TestFixtureAttribute"
+            ];
+        }
+
+        public static bool IsRunningInUnitTest
+        {
+            get
+            {
+                foreach (var f in new StackTrace().GetFrames())
+                {
+                    var customAttributes = f.GetMethod()!.DeclaringType!.GetCustomAttributes(false);
+
+                    if (customAttributes.Any(x => UnitTestingAttributes.Contains(x.GetType().FullName!)))
+                        return true;
+                }
+                return false;
+            }
+        }
+    }
+}
